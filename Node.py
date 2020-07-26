9fa492e808381ebf0047395eb866562af181bf21
NoahLipton <36777375+NoahLipton@users.noreply.github.com>
2020-07-26 16:23:40 -0400

    error on line 17 in parser.py

---
+++ Node.py
@@ -0,0 +1,9 @@
+class node:
+    space = 0  # stores count of spaces in an image
+    plus = 0  # stores count of pluses in an image
+    hashtag = 0  # stores count of hashtags in an image
+    ratio = 0 # ratio of space to nonspaces
+
+    def __init__(self, image, label):
+        self.image = image
+        self.label = label
--- parser.py
+++ parser.py
@@ -1,7 +1,32 @@
 import samples
+import Node
+
+training_images_digit = samples.loadDataFile('digitdata/trainingimages', 5000, 28, 28)
+training_labels_digit = samples.loadLabelsFile('digitdata/traininglabels', 5000)
+
+#training_images_face = samples.loadDataFile('facedata/facedatatrain', 451, 60, 74)
+#training_labels_face = samples.loadLabelsFile('facedata/facedatatrainlabels', 451)
+
+digit_training_list = []
+
+# Creates a list where each element contains the list of lists of pixels, label
+# number of spaces, hashtags, pluses, ratio of spaces vs. nonspaces
+for im, lab in zip(training_images_digit, training_labels_digit):
+    new = Node.node(im.getPixels(), lab)
+    for i in new.image:
+        for j in new.image[i]:
+            if new.image[i][j] == '':
+                new.space += 1
+            elif new.image[i][j] == '+':
+                new.plus += 1
+            else:
+                new.hashtag += 1
+            new.ratio = new.space/(new.plus + new.hashtag)
+    digit_training_list.append(new)
+
+for test in digit_training_list:
+    print("Ratio: " + str(test.ratio))
 
 
 
-items = samples.loadDataFile('digitdata/trainingimages', 100, 28, 28);
 
-print(len(items));
